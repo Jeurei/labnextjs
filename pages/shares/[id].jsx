2df828c7d9b86d556980dc44492266cf924b9f57
Daniel Read0nly <ihatemat@yandex.ru>
2021-04-28 11:47:35 +0500

    shares

---
+++ pages/shares/[id].jsx
@@ -0,0 +1,28 @@
+import InnerPageLayout from 'components/InnerPageLayout';
+import axios from 'axios';
+import { fetchDataRoute } from 'Redux/actions/actions';
+import PageBuilder from 'components/common/pageBuilder';
+
+import PropTypes from 'prop-types';
+
+const Index = ({ pageData }) => {
+  return (
+    <InnerPageLayout>
+      {pageData.page && <PageBuilder data={pageData.page} />}
+    </InnerPageLayout>
+  );
+};
+
+export const getServerSideProps = async ({ params: { id } }) => {
+  const pageData = await axios(`${fetchDataRoute}${id}`).then((res) => {
+    return res.data;
+  });
+
+  return { props: { pageData } };
+};
+
+Index.propTypes = {
+  pageData: PropTypes.objectOf(PropTypes.object).isRequired,
+};
+
+export default Index;
